aa7220d974ebcc8133c7cb4c8b2baf10195581b5
dragonbitestail <dbt@example.com>
2026-07-03 15:01:56 -0400

    Add basic rectangular outline support
    Mouse bindings for mouse button 1 down and drag now allow
    creation of red rectangle as a basic outliner PoC.
    
    Additional:
      * Now using more flexible Canvas as underlying image container instead of Label.
      * During drag a dynamically updated visual rectangle is drawn until drag end.
        At this point the final rectangle is drawn.
      * Implemented rudimentary undo bound to "ctrl-z" to delete previously created
        rectangles (LIFO order).
    
    NOTE:
      The code state is a sloppy mess and currently represents efforts to learn enough
      TKinter to ensure TKinter is the right tool for the job.
    
      Plan is to refactor the code and use classes for better separation of
      concerns.

--- menubar/file.py
+++ menubar/file.py
@@ -1,19 +1,83 @@
 from tkinter import filedialog as fd
-from tkinter import Label
+from tkinter import Canvas
+import tkinter as tk
 from PIL import ImageTk, Image
 
+
+global canvas, x_start, y_start, last_rectangle
+
+canvas = None
+last_rectangle = None
+canvas_objects = []
+
+def undo_object(event):
+    print(f"undo_object: event: {event}")
+    if canvas_objects:
+        canvas.delete(canvas_objects.pop())
+
+def mouse_1(event):
+    global x_start, y_start
+
+    x_start = event.x
+    y_start = event.y
+    print(f"mouse_1: x_start: {x_start}, y_start: {y_start}")
+
+def drag_stop(event):
+    global x_start, y_start
+
+    print(f"drag_stop: {event.x} and {event.y}")
+    rectangle = canvas.create_rectangle(x_start, y_start, event.x, event.y,
+                                        outline="red", width=4, fill=None)
+    canvas_objects.append(rectangle)
+
+    if last_rectangle is not None:
+        canvas.delete(last_rectangle)
+
+    canvas.update_idletasks()
+
+
+def drag_handler(event=None):
+    global last_rectangle
+    #print(f"{event.x} and {event.y}")
+
+    if last_rectangle is not None:
+        canvas.delete(last_rectangle)
+
+    last_rectangle = canvas.create_rectangle(x_start, y_start, event.x, event.y,
+                                             outline="red", fill=None)
+
+
 def file_open(parent):
+    global canvas
 
     # launch file open dialog to get user selected
     # image file
     file_name = fd.askopenfilename(initialdir='~/Desktop',
-                                   filetypes=[('.avif', '*.avif')])
+                                   filetypes=[('avif', '*.avif'),
+                                              ('jpg', '*.jpg'),
+                                              ('png', '*.png'),
+                                              ])
 
     print(f"selected file_name \"{file_name}\"")
 
     loaded_img = ImageTk.PhotoImage(Image.open(file_name))
 
-    label = Label(parent, image=loaded_img)
+    print((f"Creating Canvas for loaded image: width = "
+           f"\"{loaded_img.width()}\", height = \"{loaded_img.height()}\""))
 
-    label.pack()
-    parent.mainloop()
+    canvas = Canvas(parent, width=loaded_img.width(),
+                    height=loaded_img.height())
+    canvas.image = loaded_img # prevents image from getting GC'd by Python
+    canvas.create_image(10, 10, anchor=tk.NW,
+                        image=loaded_img)
+
+    canvas.pack(fill=tk.BOTH)
+
+    canvas.bind("<Button-1>", mouse_1)
+    canvas.bind("<B1-Motion>", drag_handler)
+    canvas.bind("<ButtonRelease-1>", drag_stop)
+
+    parent.bind("<Control-z>", undo_object)
+
+    parent.update_idletasks()
+    #parent.update()
